0114f77062f185ce4bf57be93f06d8c1a2db0345
mynamezhaoyu <995680821@qq.com>
2019-09-06 11:43:24 +0800

    feat:ceshi git

--- src/components/project/Addproject.vue
+++ src/components/project/Addproject.vue
@@ -22,25 +22,49 @@
         <el-row>
           <el-col :span="12">
             <el-form-item label="产品单位：" prop="units">
-              <el-input v-model="form.units" autocomplete="off" placeholder="个/箱/件" maxlength="15" show-word-limit></el-input>
+              <el-input v-model="form.units" autocomplete="off" placeholder="个/箱/件..." maxlength="15" show-word-limit></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="12">
             <el-form-item label="成本：" prop="cost">
-              <el-input v-model="form.cost" autocomplete="off" maxlength="15" show-word-limit></el-input>
+              <el-input v-model="form.cost" autocomplete="off" maxlength="15" show-word-limit placeholder="成本"></el-input>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="12">
             <el-form-item label="售价：" prop="price">
-              <el-input v-model="form.price" autocomplete="off" maxlength="15" show-word-limit></el-input>
+              <el-input v-model="form.price" autocomplete="off" maxlength="15" show-word-limit placeholder="建议售价"></el-input>
             </el-form-item>
           </el-col>
-          <el-col :span="12">
-            <el-form-item label="产品图片：" prop="photo">
-              <el-input v-model="form.photo" autocomplete="off"></el-input>
-            </el-form-item>
+        </el-row>
+        <el-row>
+          <el-col :span="4">
+            <el-form-item label="产品图片：" prop="price"> </el-form-item>
+          </el-col>
+          <el-col :span="18" style="margin-left: -20px">
+            <el-upload
+              class="upload-demo"
+              ref="upload"
+              action="http://localhost:8000/uploadfiles"
+              :on-remove="handleRemove"
+              :multiple="true"
+              :on-change="uploadChange"
+              :file-list="fileList"
+              list-type="picture"
+              :limit="5"
+              :on-success="handleAvatarSuccess"
+              :on-exceed="
+                () => {
+                  $alert('一个产品最多上传5张图片!', '提示');
+                }
+              "
+              :auto-upload="false"
+            >
+              <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+              <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+              <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+            </el-upload>
           </el-col>
         </el-row>
       </el-form>
@@ -52,72 +76,76 @@
   </div>
 </template>
 <script>
-import city from '../../global/city.js';
 export default {
   data: function() {
     return {
       form: {
         name: '',
         sort: '',
-        units: '',
-        cost: '',
-        price: '',
-        photo: ''
+        units: '个',
+        cost: 0,
+        price: 0
       },
       rules: {
-        name: [
-          // {required: true, message: '请输入客户名称', trigger: 'blur' },
-          //     {min: 2, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur' }
-          // ],
-          // phone: [{required: true, message: '请输入联系方式', trigger: 'change' }],
-          // address: [{required: true, message: '请选择省区市', trigger: 'change' }],
-          // detailAddress: [{required: true, message: '请输入详细地址', trigger: 'change' }
-        ]
-      }
+        name: [{ required: true, message: '请输入产品名称', trigger: 'blur' }, { min: 2, max: 20, message: '长度在 2 到 20 个字符', trigger: 'blur' }],
+        sort: [{ required: true, message: '请选择产品分类', trigger: 'blur' }]
+      },
+      fileList: [],
+      fileCacheList: [],
+      xianmu: [{name: '羽毛球', value: 1}, {name: '田径', value: 2}],
+      list: [
+        { data: xianmu, data1: [], dataValue: 1, data1Value: 1 }
+      ]
     };
   },
   props: {
     dialogFormVisible: {
+      // 控制dialog显示隐藏
       type: Boolean,
       default: false
     },
     dialogType: {
+      // false 新增 true 修改
       type: Boolean,
       default: false
     },
     editData: {
+      // 修改传过来的数据
       type: Object,
       default: {}
     },
     treeData: {
+      // 产品分类的数据
       type: Array,
       default: []
     }
   },
-  watch: {
-    editData: {
-      handler(val) {
-        if (!this.dialogType) {
-          Object.keys(this.form).map(r => {
-            this.form[r] = r === 'address' ? ['340000', '340200', '340225'] : '';
-          });
-          this.$nextTick(() => {
-            if (this.$refs['ruleForm']) this.$refs['ruleForm'].resetFields();
-          });
-        } else {
-          Object.keys(this.form).map(r => {
-            this.form[r] = r === 'address' ? val[r].split(',') : val[r];
-          });
-        }
-      },
-      immediate: true
-    }
-  },
   methods: {
+    uploadChange(file) {
+      const format = ['image/jpeg', 'image/png'].includes(file.raw.type);
+      const size = file.size / 1024 < 500;
+      if (!format) {
+        this.$message.error('产品图片只能是 JPG,PNG 格式!');
+        this.$refs.upload.handleRemove(file);
+        return;
+      }
+      if (!size) {
+        this.$message.error('产品图片大小不能超过 500kb!');
+        this.$refs.upload.handleRemove(file);
+        return;
+      }
+    },
     hideDialog(type = false) {
       this.$emit('dialog', type);
     },
-    confirm() {
+    submitUpload() {
+      this.$refs.upload.submit();
+    },
+    handleAvatarSuccess(data) {
+      this.fileCacheList.push({ name: data.file.name, url: data.file.path });
+      console.log(this.fileCacheList);
+    },
+    async confirm() {
       this.$refs['ruleForm'].validate(valid => {
         if (!valid) return;
         let location = this.dialogType ? '/editCust' : '/addCust';
@@ -136,11 +164,6 @@ export default {
           this.hideDialog(true);
         });
       });
-    }
-  },
-  computed: {
-    cityData() {
-      return city || [];
     }
   },
   mounted() {}
