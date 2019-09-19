761d5f66605de99d29713362b0d95490d5b09e06
mynamezhaoyu <995680821@qq.com>
2019-09-19 18:30:35 +0800

    feat:代码提交

--- src/components/page/Dashboard.vue
+++ src/components/page/Dashboard.vue
@@ -1,343 +1,117 @@
 <template>
-  <div>
-    <el-row :gutter="20">
-      <el-col :span="8">
-        <el-card shadow="hover" class="mgb20" style="height:252px;">
-          <div class="user-info">
-            <img src="../../assets/img/img.jpg" class="user-avator" alt />
-            <div class="user-info-cont">
-              <div class="user-info-name">{{ name }}</div>
-              <div>{{ role }}</div>
+  <div class="dashboard">
+    <div class="page-header">
+      <p class="f-16 f-w">
+        工作台
+      </p>
+      <div class="page-header-content">
+        <el-row :gutter="10" class="d-f a-i-c">
+          <el-col :span="18" class="d-f a-i-c">
+            <div class="avatar-image">
+              <img src="../../assets/img/Bia.png" alt="" />
             </div>
-          </div>
-          <div class="user-info-list">
-            上次登录时间：
-            <span>2019-09-11</span>
-          </div>
-        </el-card>
-        <el-card shadow="hover" style="height:252px;">
-          <div slot="header" class="clearfix">
-            <span>客户排行榜</span>
-          </div>
-          赵宇 <el-progress :percentage="71.3" color="#42b983"></el-progress> 王三 <el-progress :percentage="24.1" color="#f1e05a"></el-progress> 胡绿叶<el-progress
-            :percentage="3.7"
-          ></el-progress>
-          王先生<el-progress :percentage="0.9" color="#f56c6c"></el-progress>
-        </el-card>
-      </el-col>
-      <el-col :span="16">
-        <el-row :gutter="20" class="mgb20">
-          <el-col :span="8">
-            <el-card shadow="hover" :body-style="{ padding: '0px' }">
-              <div class="grid-content grid-con-1">
-                <i class="el-icon-s-marketing grid-con-icon"></i>
-                <div class="grid-cont-right">
-                  <div class="grid-num">1234</div>
-                  <div>订单</div>
-                </div>
-              </div>
-            </el-card>
+            <div class="ml-20">
+              <p class="f-20 c-666">早安，赵宇，祝你开心每一天！</p>
+              <p class="f-18 c-999">
+                职务
+                <el-divider direction="vertical"></el-divider>
+                总经理
+              </p>
+            </div>
           </el-col>
-          <el-col :span="8">
-            <el-card shadow="hover" :body-style="{ padding: '0px' }">
-              <div class="grid-content grid-con-2">
-                <i class="el-icon-s-claim grid-con-icon"></i>
-                <div class="grid-cont-right">
-                  <div class="grid-num">321</div>
-                  <div>客户</div>
-                </div>
-              </div>
-            </el-card>
+          <el-col :span="6" class="d-f a-i-c j-c-f-e mr-20">
+            <ul class="mr-10">
+              <li class="f-20">订单数</li>
+              <li class="f-20 f-w t-c">321</li>
+            </ul>
+            <el-divider direction="vertical"></el-divider>
+            <ul class="ml-10">
+              <li class="f-20">销售排名</li>
+              <li class="f-20 f-w t-c">8/24</li>
+            </ul>
           </el-col>
-          <el-col :span="8">
-            <el-card shadow="hover" :body-style="{ padding: '0px' }">
-              <div class="grid-content grid-con-3">
-                <i class="el-icon-s-data grid-con-icon"></i>
-                <div class="grid-cont-right">
-                  <div class="grid-num">5000</div>
-                  <div>产品</div>
-                </div>
-              </div>
+        </el-row>
+      </div>
+    </div>
+    <el-row class="mt-20">
+      <el-col :span="15" class="card">
+        <div class="d-f j-c-s-b a-i-c mlr-10">
+          <span class="pt-15 pb-15">进行中的项目</span>
+          <el-button type="text">全部项目</el-button>
+        </div>
+        <el-row :gutter="0">
+          <el-col :span="8" v-for="item in 3" :key="item">
+            <el-card shadow="hover">
+              <i class="el-icon-apple"></i>
             </el-card>
           </el-col>
         </el-row>
-        <el-card shadow="hover" style="height:403px;">
-          <div slot="header" class="clearfix">
-            <span>待办事项</span>
-            <el-button style="float: right; padding: 3px 0" type="text">添加</el-button>
-          </div>
-
-          <el-table :data="todoList" :show-header="false" height="304" style="width: 100%;font-size:14px;">
-            <el-table-column width="40">
-              <template slot-scope="scope">
-                <el-checkbox v-model="scope.row.status"></el-checkbox>
-              </template>
-            </el-table-column>
-            <el-table-column>
-              <template slot-scope="scope">
-                <div class="todo-item" :class="{ 'todo-item-del': scope.row.status }">{{ scope.row.title }}</div>
-              </template>
-            </el-table-column>
-            <el-table-column width="60">
-              <template>
-                <i class="el-icon-edit"></i>
-                <i class="el-icon-delete"></i>
-              </template>
-            </el-table-column>
-          </el-table>
-        </el-card>
-      </el-col>
-    </el-row>
-    <el-row :gutter="20">
-      <el-col :span="12">
-        <el-card shadow="hover">
-          <schart ref="bar" class="schart" canvasId="bar" :data="data" type="bar" :options="options"></schart>
-        </el-card>
-      </el-col>
-      <el-col :span="12">
-        <el-card shadow="hover">
-          <schart ref="line" class="schart" canvasId="line" :data="data" type="line" :options="options2"></schart>
-        </el-card>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-import Schart from 'vue-schart';
 import bus from '../common/bus';
 export default {
   name: 'dashboard',
   data() {
-    return {
-      name: localStorage.getItem('ms_username'),
-      todoList: [
-        {
-          title: '今天给泥汊的王先生送货',
-          status: false
-        },
-        {
-          title: '今天给泥汊的王先生送货',
-          status: false
-        },
-        {
-          title: '今天给泥汊的王先生送货',
-          status: false
-        },
-        {
-          title: '今天给泥汊的王先生送货',
-          status: false
-        },
-        {
-          title: '今天给泥汊的王先生送货',
-          status: true
-        },
-        {
-          title: '今天给泥汊的王先生送货',
-          status: true
-        }
-      ],
-      data: [
-        {
-          name: '2018/09/04',
-          value: 1083
-        },
-        {
-          name: '2018/09/05',
-          value: 941
-        },
-        {
-          name: '2018/09/06',
-          value: 1139
-        },
-        {
-          name: '2018/09/07',
-          value: 816
-        },
-        {
-          name: '2018/09/08',
-          value: 327
-        },
-        {
-          name: '2018/09/09',
-          value: 228
-        },
-        {
-          name: '2018/09/10',
-          value: 1065
-        }
-      ],
-      options: {
-        title: '最近七天新增订单',
-        showValue: false,
-        fillColor: 'rgb(45, 140, 240)',
-        bottomPadding: 30,
-        topPadding: 30
-      },
-      options2: {
-        title: '最近七天新增产品',
-        fillColor: '#FC6FA1',
-        axisColor: '#008ACD',
-        contentColor: '#EEEEEE',
-        bgColor: '#F5F8FD',
-        bottomPadding: 30,
-        topPadding: 30
-      }
-    };
+    return {};
   },
-  components: {
-    Schart
-  },
-  computed: {
-    role() {
-      return this.name === 'admin' ? '超级管理员' : '普通用户';
-    }
-  },
-  created() {
-    this.handleListener();
-    this.changeDate();
-  },
-  activated() {
-    this.handleListener();
-  },
-  deactivated() {
-    window.removeEventListener('resize', this.renderChart);
-    bus.$off('collapse', this.handleBus);
-  },
-  methods: {
-    changeDate() {
-      const now = new Date().getTime();
-      this.data.forEach((item, index) => {
-        const date = new Date(now - (6 - index) * 86400000);
-        item.name = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
-      });
-    },
-    handleListener() {
-      bus.$on('collapse', this.handleBus);
-      // 调用renderChart方法对图表进行重新渲染
-      window.addEventListener('resize', this.renderChart);
-    },
-    handleBus(msg) {
-      setTimeout(() => {
-        this.renderChart();
-      }, 300);
-    },
-    renderChart() {
-      this.$refs.bar.renderChart();
-      this.$refs.line.renderChart();
-    }
-  }
+  components: {},
+  computed: {},
+  created() {},
+  methods: {}
 };
 </script>
 
 
-<style scoped>
-.el-row {
-  margin-bottom: 20px;
-}
-
-.grid-content {
-  display: flex;
-  align-items: center;
-  height: 100px;
-}
-
-.grid-cont-right {
-  flex: 1;
-  text-align: center;
+<style lang="stylus" scoped>
+.page-header {
+  box-sizing: border-box;
+  margin: 0;
+  color: rgba(0, 0, 0, 0.65);
   font-size: 14px;
-  color: #999;
+  font-variant: tabular-nums;
+  line-height: 1.5;
+  list-style: none;
+  font-feature-settings: 'tnum';
+  position: relative;
+  padding: 16px 14px;
+  background: #fff;
+
+  .page-header-content {
+    margin-top: 10px;
+
+    .avatar-image {
+      width: 72px;
+      height: 72px;
+      border-radius: 72px;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
 }
 
-.grid-num {
-  font-size: 30px;
-  font-weight: bold;
+.card {
+  background: #fff;
+  border-radius: 5px;
 }
+</style>
+<style lang="stylus">
+.dashboard {
+  .el-divider--vertical {
+    height: 40px;
+  }
 
-.grid-con-icon {
-  font-size: 50px;
-  width: 100px;
-  height: 100px;
-  text-align: center;
-  line-height: 100px;
-  color: #fff;
-}
-
-.grid-con-1 .grid-con-icon {
-  background: rgb(45, 140, 240);
-}
-
-.grid-con-1 .grid-num {
-  color: rgb(45, 140, 240);
-}
-
-.grid-con-2 .grid-con-icon {
-  background: rgb(100, 213, 114);
-}
-
-.grid-con-2 .grid-num {
-  color: rgb(45, 140, 240);
-}
-
-.grid-con-3 .grid-con-icon {
-  background: rgb(242, 94, 67);
-}
-
-.grid-con-3 .grid-num {
-  color: rgb(242, 94, 67);
-}
-
-.user-info {
-  display: flex;
-  align-items: center;
-  padding-bottom: 20px;
-  border-bottom: 2px solid #ccc;
-  margin-bottom: 20px;
-}
-
-.user-avator {
-  width: 120px;
-  height: 120px;
-  border-radius: 50%;
-}
-
-.user-info-cont {
-  padding-left: 50px;
-  flex: 1;
-  font-size: 14px;
-  color: #999;
-}
-
-.user-info-cont div:first-child {
-  font-size: 30px;
-  color: #222;
-}
-
-.user-info-list {
-  font-size: 14px;
-  color: #999;
-  line-height: 25px;
-}
-
-.user-info-list span {
-  margin-left: 60px;
-}
-
-.mgb20 {
-  margin-bottom: 20px;
-}
-
-.todo-item {
-  font-size: 14px;
-}
-
-.todo-item-del {
-  text-decoration: line-through;
-  color: #999;
-}
-
-.schart {
-  width: 100%;
-  height: 300px;
+  .el-card, .el-message {
+    box-shadow: 1px 0 0 0 #e8e8e8, 0 1px 0 0 #e8e8e8, 1px 1px 0 0 #e8e8e8, inset 1px 0 0 0 #e8e8e8, inset 0 1px 0 0 #e8e8e8;
+    border-radius: 0;
+    border: 0;
+    padding: 24px;
+  }
 }
 </style>
